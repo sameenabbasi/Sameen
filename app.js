af18129a38e37f2d6cde174921955726aa8ee356
sameenabbasi <sameenabbasi@icloud.com>
2022-08-08 20:17:26 -0400

    Added affects to heading.

---
+++ app.js
@@ -0,0 +1,25 @@
+
+let speed = 400;
+let text = "Hi, I'm Sameen."
+let i = 0;
+
+document.body.onload = function() {
+    typewriter();
+    blink();
+}
+
+function typewriter() {
+    if (i < text.length) {
+        document.getElementById("typewriter").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typewriter, speed);
+    }
+} 
+function blink() {
+    document.getElementById("blink").style.visibility = "visible";
+    setTimeout(notblink, 600);
+}
+function notblink() {
+    document.getElementById("blink").style.visibility = "hidden";
+    setTimeout(blink, 600);
+}
